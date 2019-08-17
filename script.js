bf722caac39ba660af7ea23c2ddb075d0f1f3d9c
ilyakoptev <ilya.koptev@gmail.com>
2019-08-17 17:18:52 +0300

    set up image change to 2 sec and update function

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
- function changeImage() {
-     var timerid = 0;
-     var images = new Array("images/image1.jpg",
-         "images/image2.jpg",
-         "images/image3.jpg",
-         "images/image4.jpg");
-     var countimages = 0;
+function changeImage() {
 
-     function startTime() {
-         if (timerid) {
-             timerid = 0;
-         }
-         var tDate = new Date();
+    var images = new Array("images/image1.jpg",
+        "images/image2.jpg",
+        "images/image3.jpg",
+        "images/image4.jpg");
+    var countimages = 0;
 
-         if (countimages == images.length) {
-             countimages = 0;
-         }
-         if (tDate.getSeconds() % 2 == 0) {
-             document.getElementById("img1").src = images[countimages];
-         }
-         countimages++;
+    function startTime() {
+        if (countimages == images.length) {
+            countimages = 0;
+        }
+        document.getElementById("img1").src = images[countimages];
+        countimages++;
 
-         timerid = setTimeout("startTime()", 1000);
-     }
-     window.setInterval(startTime, 2000);
- }
+    }
+    window.setInterval(startTime, 2000);
+}
